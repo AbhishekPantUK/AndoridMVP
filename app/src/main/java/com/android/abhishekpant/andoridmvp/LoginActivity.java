9e6d23cb50ecd4096ed9404b953eaf97ca920cfc
abhishekpant <apant9972@gmail.com>
2018-08-12 15:24:24 +0530

    removed unused variables

--- app/src/main/java/com/android/abhishekpant/andoridmvp/LoginActivity.java
+++ app/src/main/java/com/android/abhishekpant/andoridmvp/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
@@ -17,13 +16,7 @@ import com.android.abhishekpant.andoridmvp.view.ILoginView;
  */
 public class LoginActivity extends AppCompatActivity implements ILoginView {
 
-
     // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
-
     private ProgressBar progressBar;
     private EditText username;
     private EditText password;
